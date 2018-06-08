24a2b3437cdbd6da932c1fd8265784714ca29ca8
Clovis Shropshire <shropshireclovis@gmail.com>
2018-06-08 11:33:31 -0400

    Initial check-in of module DigitalB

---
+++ DigitalB/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V4.View;
+
+namespace DigitalB
+{
+	[Activity(Label = "DigitalB", MainLauncher = true, Icon = "@mipmap/icon", Theme = "@style/Theme.AppCompat.Light.NoActionBar")]
+	public class MainActivity : Android.Support.V4.App.FragmentActivity
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+			ViewPager viewPager = FindViewById<ViewPager>(Resource.Id.viewPager1);
+
+			viewPager.Adapter = new OnboardingPagerAdapter(SupportFragmentManager);
+
+			var transaction = SupportFragmentManager.BeginTransaction();
+			transaction.Replace(Resource.Id.viewPager1, new OnboardingOneFragment());
+			transaction.Commit();
+        }
+    }
+}
+
---
+++ DigitalB/OnboardingPagerAdapter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Android.Support.V4.App;
+
+namespace DigitalB
+{
+	public class OnboardingPagerAdapter : FragmentPagerAdapter
+    {
+
+		public OnboardingPagerAdapter(FragmentManager fm) : base(fm)
+		{
+		}
+
+		public override int Count {
+			get {
+				return 3;
+			}
+		}
+
+		public override Fragment GetItem(int position)
+		{
+			switch (position)
+			{
+				case 0: 
+					return new OnboardingOneFragment();
+					break;
+				case 1:
+					return new OnboardingTwoFragment();
+					break;
+				default:
+					return new OnboardingOneFragment();
+					break;
+			}
+		}
+	}
+}
